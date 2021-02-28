a2ac88654d6262eca107e176ee3f9c2a262a1818
amir-ghaderian <aghaderiyan.8503@yahoo.com>
2021-02-28 14:03:25 +0330

    new-detail

---
+++ image/first.js
@@ -0,0 +1,20 @@
+//variable
+//case- sensitive
+
+//1.DECLARATION
+let Visitor
+//2.DEFUINTION
+Visitor=18
+
+
+alert (Visitor)
+
+//constant
+const p =3.14
+console.log(p)
+
+let name="amir"
+let family='ghaderian'
+let website=`www.sormeh-bazar.com`
+
+console.log(typeof(p))
