5713dd42f531a1bf32290aabedfc67dbd6478d89
idaamiri <iamidaamiri80@gmail.com>
2021-04-24 10:35:40 -0700

    js

---
+++ develop/js practices/practice.js
@@ -0,0 +1,13 @@
+var cars = ["audi" , "BMW" , "Benz" , "ferrari"]
+console.log(cars)
+
+var foods = ["pizza" , "sandwich" , "soup"]
+console.log(foods)
+
+var workers = ["ali" , "hasan" , "hosein"]
+console.log(workers)
+
+var Specifications = {firstName:"ida" , lastName:"amiri" , age:19 , homeTown:"Iran"};
+console.log(Specifications)
+
+
--- develop/twitter/cook.js
+++ develop/twitter/cook.js
@@ -1,8 +1,2 @@
-var user=[{name:'amir',food:'kfc'}];
 
-[{name:'ida',food:'soup'}];
-
-[{name:'houman',food:'ramen'}];
-
-console.log(user)
 
