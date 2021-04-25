9f7e84fd6a7135a1d6778f3c7724266cf2831702
idaamiri <iamidaamiri80@gmail.com>
2021-04-25 02:31:33 -0700

    js practice

--- develop/js practices/practice.js
+++ develop/js practices/practice.js
@@ -10,4 +10,23 @@ console.log(workers)
 var Specifications = {firstName:"ida" , lastName:"amiri" , age:19 , homeTown:"Iran"};
 console.log(Specifications)
 
+var homeTown = "Iran";
 
+if (homeTown == "America"){console.log("you are qualafied");}
+
+
+else if (homeTown == "Germany"){console.log("you are qualafied");}
+
+
+else {console.log("you are not qualafied");}
+
+
+var age =19;
+
+while (age < 18);
+
+console.log("not qualafied");
+
+while(age > 18);{
+
+console.log("qualafied");}
