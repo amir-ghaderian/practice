f2ffc304e852e94652fe41b230deb0c6813e2a51
idaamiri <iamidaamiri80@gmail.com>
2021-04-25 15:18:33 -0700

    arrays

--- develop/js practices/practice.js
+++ develop/js practices/practice.js
@@ -1,32 +1,79 @@
-var cars = ["audi" , "BMW" , "Benz" , "ferrari"]
-console.log(cars)
+var cars = ["audi", "BMW", "Benz", "ferrari"];
+console.log(cars);
 
-var foods = ["pizza" , "sandwich" , "soup"]
-console.log(foods)
+var foods = ["pizza", "sandwich", "soup"];
+console.log(foods);
 
-var workers = ["ali" , "hasan" , "hosein"]
-console.log(workers)
+var workers = ["ali", "hasan", "hosein"];
+console.log(workers);
 
-var Specifications = {firstName:"ida" , lastName:"amiri" , age:19 , homeTown:"Iran"};
-console.log(Specifications)
+var Specifications = {
+  firstName: "ida",
+  lastName: "amiri",
+  age: 19,
+  homeTown: "Iran",
+};
+console.log(Specifications);
 
 var homeTown = "Iran";
 
-if (homeTown == "America"){console.log("you are qualafied");}
+if (homeTown == "America") {
+  console.log("you are qualafied");
+} else if (homeTown == "Germany") {
+  console.log("you are qualafied");
+} else {
+  console.log("you are not qualafied");
+}
+var i = '4';
+while (i.length < 14) {
+    i = i + '@'
+    console.log(i);
+}
 
 
-else if (homeTown == "Germany"){console.log("you are qualafied");}
 
+var aArray = ["mohammad" , "amin" , "hasan" , "hosein"];
+console.log(aArray);
 
-else {console.log("you are not qualafied");}
+var bArray = ["mohammad" , "amin" , "hasan" , "hosein"];
+console.log(bArray.pop());
 
+var cArray = ["mohammad" , "amin" , "hasan" , "hosein"];
+cArray.push("ali");
+console.log(cArray);
 
-var age =19;
+var dArray = ["mohammad" , "amin" , "hasan" , "hosein"];
+console.log(cArray.shift());
 
-while (age < 18);
+var eArray = ["mohammad" , "amin" , "hasan" , "hosein"];
+eArray.shift(eArray);
+console.log(eArray);
 
-console.log("not qualafied");
+var fArray = ["mohammad" , "amin" , "hasan" , "hosein"];
+fArray.pop(fArray);
+console.log(fArray);
 
-while(age > 18);{
+var gArray = ["mohammad" , "amin" , "hasan" , "hosein"];
+gArray.unshift("amir");
+console.log(gArray);
 
-console.log("qualafied");}
+var hArray = ["mohammad" , "amin" , "hasan" , "hosein"];
+delete hArray[1];
+console.log(hArray);
+
+var iArray = ["mohammad" , "amin" , "hasan" , "hosein"];
+iArray.reverse(iArray);
+console.log(iArray);
+
+var jArray = ["mohammad" , "amin" , "hasan" , "hosein"];
+jArray.push("hamid");
+console.log(jArray.sort());
+
+var kArray = ["mohammad" , "amin" , "hasan" , "hosein"];
+kArray.push("ida");
+kArray.reverse(kArray);
+console.log(kArray.slice(2));
+
+var mArray = ["mohammad" , "amin" , "hasan" , "hosein"];
+var nArray = ["volvo" , "ferrari" , "benz" , "BMW"];
+console.log(mArray.concat(nArray));
