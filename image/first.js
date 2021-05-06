30ed1c7b3e2706cf24b8b0b93892ab54bde4ad4c
amir-ghaderian <aghaderiyan.8503@yahoo.com>
2021-05-06 11:34:48 +0330

    box1 set align follower  remove border pic box3  and js

--- image/first.js
+++ image/first.js
@@ -1,5 +1,21 @@
-var a=2;
-for(var i=0 ; i < 5; i++){
-a++
-}
-alert(a);
+// var a=2;
+// for(var i=0 ; i < 5; i++){
+// a++
+// }
+// alert(a);
+// var x=Math.round(3.657758);
+// console.log(x);
+// var x=Math.pow(2,4);
+// alert(x);
+// var x= Math.min(1,43,7,68,7,98,8,0,90,9,9,0,0,9,076,8,67,5,6,7,);
+// console.log(x);
+// var h=Math.floor(h=Math.random(23)*14);
+// console.log(h);
+// var txt='amir hast ye oshgool';
+// console.log(txt.indexOf('oshgool'));
+var txt="انواع مانتو جدید ارزان قیمت شیک رسمی اسپرت اداری پاییزه بهاره مدل کوتاه بلند گرم کیفیت بی نظیر وارداتی کم هزینه استانبول گرگا";
+// console.log(txt.search('اداری'));
+// console.log(txt.slice(-42,-7));
+// console.log(txt.substring(5,34));
+// console.log(txt.toLocaleUpperCase())
+console.log(txt.trim())
