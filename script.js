d138e1b9b5badc7f1b688c423122fe82c5f990e9
amir-ghaderian <aghaderiyan.8503@yahoo.com>
2021-05-18 12:36:19 +0330

    addEventListener  learing

--- script.js
+++ script.js
@@ -3,19 +3,32 @@ console.log(myptag);
 console.log(myptag.item(1).innerHTML);
 console.log(myptag.length);
 console.log(myptag.namedItem("mytxt"));
-var x=myptag.namedItem("mytxt");
+var x = myptag.namedItem("mytxt");
 console.log(x);
 console.log(document.getElementsByTagName("*"));
 function myDown(event) {
-   // var txt =document.getElementById("name");
-    txt.innerHTML=event.code;
+    var txt = document.getElementById("name");
+    txt.innerHTML = event.code;
     console.log(event);
     console.log(event.target);
     console.log(event.target.nodeName);
 }
 function myClick() {
-    var txt =document.getElementById("box");
+    var txt = document.getElementById("box");
     // console.log(event);
     // console.log(event.screenX);
-    txt.innerHTML=event.clientX;
+    txt.innerHTML = event.clientX;
 }
+function myover() {
+    var txt = document.getElementById("box1");
+    txt.innerHTML = event.screenY;
+
+}
+
+
+
+var ptxt = document.getElementById("ptxt");
+var name = document.getElementById("name1");
+document.getElementById("name1").addEventListener("keydown", function () {
+    document.getElementById("ptxt").innerHTML = document.getElementById("name1").value;
+});
