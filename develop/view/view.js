a7435f7619e935b65b4f53f216dc2be665542086
idaamiri <iamidaamiri80@gmail.com>
2021-05-24 09:30:51 -0700

    json lang

--- develop/view/view.js
+++ develop/view/view.js
@@ -1,11 +1,8 @@
-var paragraph = document.createElement("p1");
-var node = document.createTextNode("This is new p ");
-paragraph.appendChild(node);
-var element = document.getElementById("div1");
-element.appendChild(paragraph);
 
-var para = document.createElement("p3");
-var node1 = document.createTextNode("wellcome");
-para.insertBefore(node1);
-var trick = document.getElementById("slight");
-trick.insertBefore(para,child);
+var attendees =
+  '{"workers" :[' +
+  '{"firstName":"ahmad" , "lastName":"mohammadi" , "age":23},' +
+  '{"firstName":"hossein" , "lastName":"ahmadi" , "age":13},]}';
+  var obj = JSON.parse(attendees);
+  document.getElementById("max").innerHTML =
+    obj.workers[1].firstName + "  last name is:  " + obj.workers[1].lastName;
