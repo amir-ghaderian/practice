8f11359b44a4ef06ebeabc05aa071633c1ef6211
idaamiri <iamidaamiri80@gmail.com>
2021-05-24 13:12:00 -0700

    make class in js

--- develop/view/view.js
+++ develop/view/view.js
@@ -1,8 +1,17 @@
-
 var attendees =
   '{"workers" :[' +
   '{"firstName":"ahmad" , "lastName":"mohammadi" , "age":23},' +
   '{"firstName":"hossein" , "lastName":"ahmadi" , "age":13},]}';
-  var obj = JSON.parse(attendees);
-  document.getElementById("max").innerHTML =
-    obj.workers[1].firstName + "  last name is:  " + obj.workers[1].lastName;
+var obj = JSON.parse(attendees);
+document.getElementById("max").innerHTML =
+  obj.workers[1].firstName + "  last name is:  " + obj.workers[1].lastName;
+
+class trick {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+}
+myTrick = new trick("ida", 2001);
+document.getElementById("grand").innerHTML = myTrick.name + "" + myTrick.year;
+
