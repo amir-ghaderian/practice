bde92c6699e602ec25d222eb6e46e30802fe5785
idaamiri <iamidaamiri80@gmail.com>
2021-05-31 13:25:06 -0700

    set time

---
+++ develop/tx/tx.js
@@ -0,0 +1,14 @@
+//var myPTag = document.createElement("p");
+//var myText = document.createTextNode("hello every body");
+//myPTag.appendChild(myText);
+
+//var firstText = document.body.getElementsByTagName("p")[0];
+//document.body.removeChild(firstText);
+
+var rainBow = new Date();
+var myDay = rainBow.getHours();
+document.write(myDay);
+var mins = rainBow.getMinutes();
+document.write(mins);
+var sec = rainBow.getSeconds();
+document.write(sec);
