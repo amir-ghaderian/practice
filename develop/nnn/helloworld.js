2fa63666b62cd23c52b38b85eade379acb4812e7
idaamiri <iamidaamiri80@gmail.com>
2021-06-15 10:23:45 -0700

    create element by js

---
+++ develop/nnn/helloworld.js
@@ -0,0 +1,16 @@
+//var myDay = Date();
+//document.write(myDay);
+//function myDay1(myCar, myPlace) {
+//let myCar = "BMW";
+//let myPlace = "Melborn";
+//console.log("my car is" + myCar + "and i drive to " + myPlace + ;
+//}
+//function myNewInterval() {
+//window.alert("hi");
+//}
+//window.setInterval("myNewInterval", 10000);
+var myLi = document.getElementById("myLi");
+var createLi = document.createElement("li");
+var newLi = document.createTextNode("6");
+createLi.appendChild(newLi);
+myLi.appendChild(createLi);
